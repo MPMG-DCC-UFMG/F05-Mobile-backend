fc83d3b3c4afcf6bdb154f1192a44da54715d697
Christian Noriega <ufmg.cnoriega@backendapp-ufmg-f05.mpmg.mp.br>
2022-07-21 09:11:18 -0300

    chore: envs parameters

--- app/main.py
+++ app/main.py
@@ -6,4 +6,4 @@ from application import mp_backend
 if __name__ == '__main__':
     settings = mp_backend.config.settings
     sentry_sdk.init(settings.sentry_key, environment=settings.environment)
-    uvicorn.run(mp_backend.mpApi, host="0.0.0.0", port=8000, root_path=mp_backend.config.settings.api_prefix)
+    uvicorn.run(mp_backend.mpApi, host="0.0.0.0", port=80, root_path=mp_backend.config.settings.api_prefix)
